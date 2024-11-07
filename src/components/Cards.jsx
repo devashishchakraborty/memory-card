aa1245da0fafa237d729eb623445d37804026233
Devashish Chakraborty <devashishchakra@gmail.com>
2024-11-08 03:05:19 +0530

    Added a function to try and shuffle cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,6 +2,21 @@ import "../styles/Cards.css"
 import { useState } from "react" 
 
 function Cards({ cards, setCards, setCurrentScore }) {
+
+    function shuffleCards(cards){
+        for (let i = cards.length - 1; i > 0; i--) {
+            // Generate a random index from 0 to i
+            const j = Math.floor(Math.random() * (i + 1));
+            // Swap elements at i and j
+            [cards[i], cards[j]] = [cards[j], cards[i]];
+          }
+          return cards;
+    }
+
+    const handleClick = () => {
+        setCards((cards) => shuffleCards(cards))
+    }
+
     return (
         <div className="cardContainer">
             {
